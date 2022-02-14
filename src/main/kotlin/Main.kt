6b1ab942279ad72318dbe411d2debd87671ac8c7
Odoyo-Queenter <achiengodoyoqueenter@gmail.com>
2022-02-14 15:34:06 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+     var names = "Queenter Achieng"
+    println(names)
+    var age = "24"
+    println(age)
+    var weight = "50.45"
+    println(weight)
+    var phonenumber ="0707747965"
+    println(phonenumber)
+    val iskenyan = "true"
+    println(iskenyan)
+    val isnotkenyan = "false"
+    println(isnotkenyan)
+}
