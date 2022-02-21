b53137ac0475138650b6baf1e35c36a346f25d37
Odoyo-Queenter <achiengodoyoqueenter@gmail.com>
2022-02-21 14:40:00 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,23 @@
 fun main() {
+    name("Queenter")
+    var y = reminder(30,4)
+    println(y)
+    var q = addition(100,200,300,400)
+    println(q)
+    fact("i am empathetic")
+}
+fun name(name: String){
+println("Hello $name")
 
-     var names = "Queenter Achieng"
-    println(names)
-    var age = "24"
-    println(age)
-    var weight = "50.45"
-    println(weight)
-    var phonenumber ="0707747965"
-    println(phonenumber)
-    val iskenyan = "true"
-    println(iskenyan)
-    val isnotkenyan = "false"
-    println(isnotkenyan)
+}
+fun reminder(numb1:Int,numb2:Int):Int {
+    var modulus = numb1 % numb2
+    return modulus
+}
+fun addition(numb1:Int,numb2:Int,numb3:Int,numb4:Int):Int{
+    var sum = numb1 + numb2 + numb3 + numb4
+    return sum
+}
+fun fact(fact:String){
+    println("i am empathetic")
 }
